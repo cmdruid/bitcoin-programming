c6fa7e0c156472c4ab337a5ca7ebcf3ef2908875
cmd <cmd@yggdrasil.onion>
2022-09-24 14:11:54 -0500

    update

--- examples/python/transactions/p2pkh.py
+++ examples/python/transactions/p2pkh.py
@@ -11,21 +11,17 @@ of a public key. In order to unlock the tranasction, a user
 will have to provide the un-hashed public key, along with a
 digital signature which shares that same public key.
 
-Specify a funding utxo and recipient address, then use your
-wallet to sign and broadcast the transaction. This will transfer 
-control of the funds to the pubkey hash that is encoded within 
-the recipient address. 
-
 You can generate a block in order to commit this transaction 
 to the blockchain (if you are using regtest).
 """
 
-from sys import path
+import os, sys
 
 sys.path.append(os.path.dirname(__file__).split('/transactions')[0])
 
 from lib.encoder import encode_tx
-from lib.helper import decode_address
+from lib.helper  import decode_address
+from lib.hash    import hash256
 from lib.sign    import sign_tx
 from lib.rpc     import RpcSocket
 
@@ -33,63 +29,85 @@ from lib.rpc     import RpcSocket
 rpc = RpcSocket({ 'wallet': 'regtest' })
 assert rpc.check()
 
-## First, we will lookup an existing utxo,
-## and use that to fund our transaction.
-utxo = rpc.get_utxo(0)
+## Get a utxo for Alice.
+alice_utxo = rpc.get_utxo(0)
 
-## We will also grab a new receiving address,
-## and lock the funds to this address.
-recv = rpc.get_recv()
+## Get a change address for Alice.
+alice_change_txout = rpc.get_recv(fmt='base58')
+alice_pubkey_hash  = decode_address(alice_change_txout['address'])
 
-## We decode the address to get the actual hash.
-pubkey_hash = decode_address(recv['address'])
+## Get a payment address for Bob.
+bob_payment_txout = rpc.get_recv(fmt='base58')
+bob_pubkey_hash   = decode_address(bob_payment_txout['address'])
 
-## The spending transaction. We are including a basic P2PKH script in 
-## the script_pubkey field. This script can later be unlocked by a new 
-## transaction input which provides the correct public key and signature.
-new_tx = {
+## Calculate our output amounts.
+fee = 1000
+bob_recv_value = alice_utxo['value'] // 2
+alice_change_value = alice_utxo['value'] // 2 - fee
+
+## The spending transaction.
+atob_tx = {
     'version': 1,
     'vin': [{
-        'txid': utxo['txid'],
-        'vout': utxo['vout'],
+        # We are unlocking the utxo from Alice.
+        'txid': alice_utxo['txid'],
+        'vout': alice_utxo['vout'],
         'script_sig': [],
         'sequence': 0xFFFFFFFF
     }],
-    'vout': [{
-        'value': utxo['value'] - 1000,
-        'script_pubkey': ['OP_DUP', 'OP_HASH160', pubkey_hash, 'OP_EQUALVERIFY', 'OP_CHECKSIG']
-    }],
+    'vout': [
+        {
+            'value': bob_recv_value,
+            'script_pubkey': ['OP_DUP', 'OP_HASH160', bob_pubkey_hash, 'OP_EQUALVERIFY', 'OP_CHECKSIG']
+        },
+        {
+            'value': alice_change_value,
+            'script_pubkey': ['OP_DUP', 'OP_HASH160', alice_pubkey_hash, 'OP_EQUALVERIFY', 'OP_CHECKSIG']
+        }
+    ],
     'locktime': 0
 }
 
-## Encode the transaction into raw hex,
-## and calculate the transaction ID
-locking_hex  = encode_tx(locking_tx)
-locking_txid = hash256(bytes.fromhex(locking_hex))[::-1].hex()
+## Serialize the transaction and calculate the TXID.
+atob_hex  = encode_tx(atob_tx)
+atob_txid = hash256(bytes.fromhex(atob_hex))[::-1].hex()
 
-## Sign the transaction using our key-pair from the utxo.
-redeem_script = f'76a914{pubkey_hash}88ac'
+## The redeem script is a basic Pay-to-Pubkey-Hash template.
+redeem_script = f"76a914{alice_utxo['pubkey_hash']}88ac"
 
-signature = sign_tx(
-    new_tx,
-    0,
-    utxo['value'],
-    redeem_script,
-    utxo['priv_key']
+## We are signing Alice's UTXO using BIP143 standard.
+alice_signature = sign_tx(
+    atob_tx,                # The transaction.
+    0,                      # The input being signed.
+    alice_utxo['value'],    # The value of the utxo being spent.
+    redeem_script,          # The redeem script to unlock the utxo. 
+    alice_utxo['priv_key']  # The private key to the utxo pubkey hash.
 )
 
-## Add the signature and public key to the transaction.
-locking_tx['vin'][0]['witness'] = [ signature, utxo['pub_key'] ]
+## Include the arguments needed to unlock the redeem script.
+atob_tx['vin'][0]['witness'] = [ alice_signature, alice_utxo['pub_key'] ]
 
 print(f'''
-# Pay-to-Pubkey-Hash Example
+## Pay-to-Pubkey-Hash Example ##
 
-Recipient Address:
-{recipient_address}
+-- Transaction Id --
+{atob_txid}
 
-Pubkey Hash:
-{pubkey_hash}
+-- Alice UTXO --
+     Txid : {alice_utxo['txid']}
+     Vout : {alice_utxo['vout']}
+    Value : {alice_utxo['value']}
+     Hash : {alice_utxo['pubkey_hash']}
 
-Spending Tx:
-{spending_tx}
+-- Sending to Bob --
+  Address : {bob_payment_txout['address']}
+    Coins : {bob_recv_value}
+
+-- Change --
+  Address : {alice_change_txout['address']}
+      Fee : {fee}
+    Coins : {alice_change_value}
+
+-- Hex --
+{encode_tx(atob_tx)}
 ''')
--- examples/python/transactions/p2wpkh.py
+++ examples/python/transactions/p2wpkh.py
@@ -2,52 +2,6 @@
 
 """
 Example of a Pay-to-Witness-Pubkey-Hash (P2WPKH) transaction.
-
-Pay-to-Witness is a new convention that brings major upgrades to 
-Bitcoin Script:
-
- - The "script_sig" field has been depreciated, and instead the burden 
-   of storing script code has been moved to the end of the transaction.
-   (called the "witness data")
-   
- - Locking scripts are now preceeded with a version number, meaning that
-   the script parsing engine can be upgraded to future versions.
-
- - The previously-disabled "sequence" field has been revived, and can now
-   be used to signal certain new features (RBF, OP_CHECKSEQUENCEVERIFY).
- 
- - Malleability of the transaction Id has been fixed, meaning we can now 
-   reliably sign a funding transaction and utilize it off-chain, without 
-   worrying about the funding transaction diverging from the signature.
-
- - Locking scripts are now encoded in the Bech32 address format, which
-   provides superier readability and error-detection.
-
-This moves the storage of the script program itself to the transaction 
-inputs, rather than the outputs. Bitcoin nodes need to store a list of 
-unspent transaction outputs (utxo set, or chainstate) in order to quickly 
-validate all new transactions which spend them, so moving the burden of
-program scripts (which take up the majority of a transaction) away from
-the chainstate helps with scalability.
-
-In order to unlock a P2SH transaction, the spending transaction will 
-have to provide any arguments to the script, plus the script itself.
-This input script will be hashed and checked against the hash stored
-in the output's locking script. If the hashes match, then the input
-script will be executed with the provided arguments.
-
-Specify a funding utxo, then use your wallet to 
-sign and broadcast the transaction. This will transfer control of the 
-funds to the script hash.
-
-You can generate a block in order to commit this transaction 
-to the blockchain (if you are using regtest).
-
-To unlock and spend the script back to one of our recipient addresses,
-we will create a second transaction. Since we are not checking signatures
-within our script, this second transaction does not need to be signed.
-
-(it is dangerous to lock real coins to a script that doesn't check for signatures)
 """
 
 import os, sys
@@ -55,6 +9,7 @@ import os, sys
 sys.path.append(os.path.dirname(__file__).split('/transactions')[0])
 
 from lib.encoder import encode_tx, encode_script
+from lib.hash    import hash256
 from lib.helper  import decode_address, hash_script, get_txid
 from lib.sign    import sign_tx
 from lib.rpc     import RpcSocket
@@ -65,53 +20,87 @@ assert rpc.check()
 
 ## First, we will lookup an existing utxo,
 ## and use that to fund our transaction.
-utxo = rpc.get_utxo(0)
+alice_utxo = rpc.get_utxo(0)
 
-## We will also grab a new receiving address,
-## and lock the funds to this address.
-recv = rpc.get_recv()
+## Get a change address for Alice.
+alice_change_txout     = rpc.get_recv()
+_, alice_redeem_script = decode_address(alice_change_txout['address'])
 
-## We decode the address into the witness version and program script.
-version, pubkey_hash = decode_address(recv['address'])
+## Get a payment address for Bob.
+bob_payment_txout    = rpc.get_recv()
+_, bob_redeem_script = decode_address(bob_payment_txout['address'])
 
-print(f'Witness Version: {version}')
-print(f'Witness Program: {pubkey_hash}')
+## Calculate our output amounts.
+fee = 1000
+bob_recv_value = alice_utxo['value'] // 2
+alice_change_value = alice_utxo['value'] // 2 - fee
 
 ## The initial spending transaction. This tx spends a previous utxo,
 ## and commits the funds to our P2WPKH transaction.
 
-new_tx = {
+## The spending transaction.
+atob_tx = {
     'version': 1,
     'vin': [{
-        'txid': utxo['txid'],
-        'vout': utxo['vout'],
+        # We are unlocking the utxo from Alice.
+        'txid': alice_utxo['txid'],
+        'vout': alice_utxo['vout'],
         'script_sig': [],
         'sequence': 0xFFFFFFFF
     }],
-    'vout': [{
-        'value': utxo['value'] - 1000,
-        'script_pubkey': [ version, pubkey_hash ]
-    }],
-    'locktime':0
+    'vout': [
+        {
+            'value': bob_recv_value,
+            'script_pubkey': [0, bob_redeem_script]
+        },
+        {
+            'value': alice_change_value,
+            'script_pubkey': [0, alice_redeem_script]
+        }
+    ],
+    'locktime': 0
 }
 
-## Since we have the complete transaction, we can calculate the transaction ID.
-new_hex  = encode_tx(new_tx)
-new_txid = get_txid(new_hex)
+## Serialize the transaction and calculate the TXID.
+atob_hex  = encode_tx(atob_tx)
+atob_txid = hash256(bytes.fromhex(atob_hex))[::-1].hex()
 
-## Sign the transaction using our key-pair from the utxo.
-redeem_script = f'76a914{pubkey_hash}88ac'
-signature = sign_tx(
-  new_tx, 
-  0,
-  utxo['value'], 
-  redeem_script,
-  utxo['priv_key']
+## The redeem script is a basic Pay-to-Pubkey-Hash template.
+redeem_script = f"76a914{alice_utxo['pubkey_hash']}88ac"
+
+## We are signing Alice's UTXO using BIP143 standard.
+alice_signature = sign_tx(
+    atob_tx,                # The transaction.
+    0,                      # The input being signed.
+    alice_utxo['value'],    # The value of the utxo being spent.
+    redeem_script,          # The redeem script to unlock the utxo. 
+    alice_utxo['priv_key']  # The private key to the utxo pubkey hash.
 )
 
-## Add the signature and public key to the transaction.
-new_tx['vin'][0]['witness'] = [ signature, utxo['pub_key'] ]
+## Include the arguments needed to unlock the redeem script.
+atob_tx['vin'][0]['witness'] = [ alice_signature, alice_utxo['pub_key'] ]
 
-## Since we have the complete transaction, we can calculate the transaction ID.
-print('\nTxid:\n' + new_txid)
-print('\nHex:\n' + encode_tx(new_tx))
+print(f'''
+## Pay-to-Witness-Pubkey-Hash Example ##
+
+-- Transaction Id --
+{atob_txid}
+
+-- Alice UTXO --
+     Txid : {alice_utxo['txid']}
+     Vout : {alice_utxo['vout']}
+    Value : {alice_utxo['value']}
+     Hash : {alice_utxo['pubkey_hash']}
+
+-- Sending to Bob --
+  Address : {bob_payment_txout['address']}
+    Coins : {bob_recv_value}
+
+-- Change --
+  Address : {alice_change_txout['address']}
+      Fee : {fee}
+    Coins : {alice_change_value}
+
+-- Hex --
+{encode_tx(atob_tx)}
+''')
